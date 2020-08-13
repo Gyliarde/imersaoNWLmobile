7a2c309202ae3cf3cdabc22595be49f3e094d444
Gyliarde Araujo <gyliardearaujo@Air-de-Gyliarde.meuintelbras.local>
2020-08-12 23:40:05 -0300

    ADD Chain Routes to Study Page

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -17,6 +17,10 @@ function Landing() {
         navigation.navigate('GiveClasses');
     }   
 
+    function handleNavigateToStudy() {
+        navigation.navigate('Study');
+    }   
+
     return (
         <View style={styles.container}  >
             <Image source={landingImg} style={styles.banner}/>
@@ -27,7 +31,7 @@ function Landing() {
             </Text>
 
             <View style={styles.buttonsContainer} >
-                <RectButton style={[styles.button, styles.buttonPrimary]} >
+                <RectButton onPress={handleNavigateToStudy} style={[styles.button, styles.buttonPrimary]} >
                     <Image source={studyIcon} />
                     <Text style={styles.buttonText}> Estudar </Text>
                 </RectButton>
